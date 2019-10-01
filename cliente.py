bb0ddc870007a096f96a7f7798b621dba420be34
Heimidall <pedroxlda12@gmail.com>
2019-10-01 16:54:57 -0300

    Archivos ejercicio 1

---
+++ cliente.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import socket
+import sys
+ 
+archivo = open('respuestas.txt','w')
+
+# Creando un socket TCP/IP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+# Conecta el socket en el puerto cuando el servidor esté escuchando
+#server_address = ('localhost', 5000)
+sock.connect(('localhost', 5000))
+
+#while flag=True:
+try:
+    # Enviando datos
+    message = 'Saludos desde el cliente.'#message = input('ingrese mensaje para el servidor')
+    sock.sendall(message.encode())#.enconde())
+
+    # Buscando respuesta
+    data = sock.recv(1024).decode()
+    print ('respuesta del servidor: '+data)
+    archivo.write(data+'\n')
+
+finally:
+    sock.close()
+archivo.close()
